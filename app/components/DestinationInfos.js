af98134863fcfaa847d443a2d8fcf47f6bac0ba3
Maxime Salomon <maxime@croissant.io>
2019-05-25 16:19:59 +0200

    Add DestinationInfos

--- app/components/DestinationInfos.js
+++ app/components/DestinationInfos.js
@@ -8,13 +8,40 @@ const DestinationInfos = () => {
   return (
     <DestinationInfosContainer>
         <h1>Barcelona & Tenerife</h1>
+        <DestinationInfosCities>Los Gigantes - Casa Mila - Casa Batlo</DestinationInfosCities>
+        <Line>-------------------</Line>
+        <DestinationInfosPricing>From $700 fro 5 days</DestinationInfosPricing>
+        <DestinationInfosView>View full price info</DestinationInfosView>
         <DestinationTabs/>
     </DestinationInfosContainer>
   )
 }
 
 const DestinationInfosContainer = styled.section`
-    height: 1000px;
+    padding-top: 40px;
+    height: 560px;
+    width: 90%;
+    margin: 0 auto;
+`
+
+const DestinationInfosCities = styled.p`
+    color: gray;
+    margin-top: 0px;
+`
+
+const Line = styled.p`
+  margin-top: 8px;
+  color: red;
+`
+
+const DestinationInfosPricing = styled.p`
+  margin-top: 8px;
+`
+
+const DestinationInfosView = styled.p`
+  color: #0072ef;
+  margin-top: 4px;
+  margin-bottom: 40px;
 `
 
 export default DestinationInfos;
--- app/components/DestinationTabs.js
+++ app/components/DestinationTabs.js
@@ -1,8 +1,35 @@
 import React from 'react';
 
-const DestinationTabs = () => {
-  return (
-    <p>LOL</p>
-  )
+import { TabStrip, TabStripTab, PanelBar, PanelBarItem, PanelBarUtils, Menu, MenuItem, MenuItemModel, MenuItemLink, MenuItemArrow, Splitter } from '@progress/kendo-react-layout'
+import '@progress/kendo-react-intl'
+import '@progress/kendo-react-dropdowns'
+
+
+class DestinationTabs extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            selected: 0
+        }
+    }
+    render() {
+        return (
+            <TabStrip selected={this.state.selected} onSelect={this.handleSelect}>
+                <TabStripTab title="Overview">
+                    <div>
+                        <p>Barcelona is an excellent place to discover world-class arts and culture. Bullfighting was officially banned several years ago, but the city remains rich with festivals and events. The sights in Barcelona are second to none. Don’t miss the architectural wonder, Casa Mila—otherwise known as La Pedrera. It’s a modernist apartment building that looks like something out of an expressionist painting. Make your way up to the roof for more architectural surprises. And if you like Casa Mila, you’ll want to see another one of Antoni Gaudi’s architectural masterpieces, Casa Batllo, which is located at the center of Barcelona.
+Tenerife, one of the nearby Canary Islands, is the perfect escape once you’ve had your fill of the city. In Los Gigantes, life revolves around the marina. Take a boat out in search of bottlenose dolphins and whales. For the able-bodied visitor, take a climb up “Cardiac Hill” to get a breathtaking view—and workout. While you’re in Tenerife, visit Mount Teide. It’s the highest point in Spain and the third-largest volcano in the world.</p>
+                    </div>
+                </TabStripTab>
+                <TabStripTab title="Attractions">
+                </TabStripTab>
+                <TabStripTab title="Enquiry Form">
+
+                </TabStripTab>
+                
+            </TabStrip>
+        )
+    }
 }
+
 export default DestinationTabs;
