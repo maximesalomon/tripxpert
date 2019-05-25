21b351a943ef46141ae3e5fc38f2913841d0988c
Maxime Salomon <maxime@croissant.io>
2019-05-25 14:00:40 +0200

    Add Card component

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,24 +1,57 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Card = () => {
+const Card = ({destination}) => {
   return (
-      <div >
+      <CardHover>
           <div><img width="100%" src="app/images/Gallery/Barcelona-and-Tenerife/Arc-de-Triomf,-Barcelona,-Spain_Liliya-Karakoleva.JPG"></img></div>
-          <div>
-              <div>Barcelona & Tenerife</div>
-              <div>
-                <p>Barcelona</p>
-                <p>Tenerife</p>
-                <p>Sanat Cruz</p>
-                <p>Teide</p>
+          <div class="card-infos-container">
+              <p class="card-destinations">{destination.name}</p>
+              <div class="card-destinations-more">
+                {
+                    destination.cities.map(city => {
+                        return (
+                            <p class="card-destinations-city">{city}</p>
+                        )
+                    })
+                }
+
               </div>
-              <div>
-                <div>From $700</div>
-                <div>View details</div>
-              </div>
+              <CardDetails>
+                <CardPrice>From $700</CardPrice>
+                <CardLink>View details</CardLink>
+              </CardDetails>
           </div>
-      </div>
+    </CardHover>
   )
 }
+
+const CardHover = styled.div `
+    border: 1px solid #f2f2f2;
+    box-shadow: 0px 4px 4px rgba(61, 64, 72, 0.04);
+    :hover {
+        box-shadow: 0px 4px 4px rgba(61, 64, 72, 0.23);
+    }
+`
+
+const CardDetails = styled.div `
+    display: flex;
+    flex-direction: row;
+    margin-top: 32px;
+    position: relative;
+    width: 100%;
+`
+
+const CardPrice = styled.p `
+    color: #ea0058;
+`
+const CardLink = styled.p `
+    position: absolute;
+    right: 0px;
+    color: #0072ef;
+    :hover {
+        cursor: pointer;
+    }
+`
 
 export default Card;
--- app/components/MasonryLayout.js
+++ app/components/MasonryLayout.js
@@ -2,19 +2,17 @@ import React from 'react';
 import Card from './Card';
 
 const destinations = [
-    {id: 1, name: "Barcelona"},
-    {id: 2, name: "New york"},
-    {id: 3, name: "Warsaw"}
+    { id: 1, name: "Barcelona & Tenerife", cities: ["Barcelona", "Teneriffe", "Santa Cruz", "Teide"], fromPrice: 700 },
+    { id: 2, name: "New york & East Coast", cities: ["Boston", "Niagara Falls", "NYC", "Florida"], fromPrice: 2100 },
+    { id: 3, name: "Malta & Mediterranean", cities: ["Valetta", "Birgu", "Mdina", "Gozo"], fromPrice: 1200 }
 ]
-
-console.log(destinations);
 
 const MasonryLayout = () => {
   return (
-        destinations && destinations.map(d => {
+        destinations && destinations.map(destination => {
             return (
                 <div class="card-container">
-                    <Card key={d.id} d={d}/>
+                    <Card key={destination.id} destination={destination}/>
                 </div>
             )
         })
